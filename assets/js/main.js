330927f72c7538bef29f1b4d6d112cda5016a3c9
Marilu Llamocca <maryleo.3007@gmail.com>
2017-03-13 12:35:04 -0500

    add agents buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function openCity(evt, cityName) {
     // Declare all variables
-    var i, tabcontent, tablinks;
+    var tabcontent, tablinks;
 
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -17,4 +17,23 @@ function openCity(evt, cityName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+function mostrar(event, elementId) {
+  var i, buttoncontent, buttonlinks;
+  buttoncontent = document.getElementsByClassName('buttoncontent');
+  for(var i = 0 ; i < buttoncontent.length ; i++){
+    buttoncontent[i].style.display = "none";
+  }
+  buttonlinks = document.getElementsByClassName('buttonlink');
+  for(var i = 0 ; i < buttonlinks.length ; i++){
+    buttonlinks[i].className =  buttonlinks[i].className.replace(" active", "");
+  }
+  document.getElementById(elementId).style.display = "block";
+  event.currentTarget.className += " active";
+  /*
+  document.getElementById('all').style.display = "none";
+  document.getElementById('physical').style.display = "none";
+  document.getElementById('virtual').style.display = "none";
+  document.getElementById(elementId).style.display = "block";*/
 }
