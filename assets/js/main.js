936d207c815bf003052ff2fef860db8f817fe9cb
Ana Durand <ana.dc1004@gmail.com>
2017-03-14 11:36:56 -0500

    falta maquetacion

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,25 @@
 
 var botones = document.getElementsByClassName("links");
 
-
 for(i=0; i<botones.length; i++){
   botones[i].addEventListener("click", function(index){
 
-      var contenido = document.getElementsByClassName("contenido");
-      var links = document.getElementsByClassName("links");
+      var contenido = document.getElementsByClassName("contenidoTab");
 
       for (i = 0; i < contenido.length; i++) {
         contenido[i].style.display = "none";
       }
-      for (i = 0; i < links.length; i++) {
-        links[i].className = links[i].className.replace(" active", "");
+      for (i = 0; i < botones.length; i++) {
+        botones[i].className = botones[i].className.replace("active", "");
       }
       document.getElementById(this.name).style.display = "block";
       this.className += " active";
 
-
   });
 
 }
+
+/*
 
 var buttonsAgents = document.getElementsByClassName("agents");
 var buttonContent = document.getElementById("buttoncontent");
@@ -73,3 +72,5 @@ function mostrar(){
 
 
 }
+
+*/
