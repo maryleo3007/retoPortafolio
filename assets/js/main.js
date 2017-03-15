1b8234f011fffb84f0d88fb80bb99cd5eec69883
Ana Durand <ana.dc1004@gmail.com>
2017-03-15 15:10:56 -0500

    cambios en style

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,89 @@
 function ResourcesManager(){
 
-  this.resources = [];
-  this.id = 0;
+    this.resources = [];
+    this.id = 0;
 
+    this.addResource = function(type){
 
-for(i=0; i<botones.length; i++){
-  botones[i].addEventListener("click", function(index){
+       this.resources.push({
+          id: this.id,
+          type: type,
 
-      var contenido = document.getElementsByClassName("contenidoTab");
+        });
+       this.id++;
 
-      for (i = 0; i < contenido.length; i++) {
-        contenido[i].style.display = "none";
-      }
-      for (i = 0; i < botones.length; i++) {
-        botones[i].className = botones[i].className.replace("active", "");
-      }
-      document.getElementById(this.name).style.display = "block";
-      this.className += " active";
+    }
 
-  });
+    this.printHtmlResource = function(parent){
+      parent.innerHTML = "";
+      this.resources.forEach(function(item){
+        parent.appendChild(this.createHtmlResource(item.type,item.id));
+      },this);
+    }
 
-}
-this.addResource = function(type){
+    this.createHtmlResource = function(type,id){
+        var post = document.createElement("div");
+        span.setAttribute("data-id",id);
+        var span = document.createElement("span");
+        span.innerHTML = type;
+        var eliminar = document.createElement("a");
+        eliminar.setAttribute("href","#");
+        eliminar.addEventListener("click",function(e){
+            e.preventDefault();
+            var resourceId = e.target.parent.getAttribute("data-id");
+            var postDelete = document.getElementById("id del div general");
+            postDelete.removeChild(postDelete.childNodes[resourceId]);
+            delete this.resources[resourceId];
 
-    this.resources.push({
-      id: this.id,
-      type: type,
-
-    });
-    this.id++;
-
-  }
-
-  this.printHtmlResource = function(parent){
-    parent.innerHTML = "";
-    this.resources.forEach(function(item){
-      parent.appendChild(this.createHtmlResource(item.type,item.id));
-    },this);
-  }
-
-  this.createHtmlResource = function(type,id){
-    var post = document.createElement("div");
-    span.setAttribute("data-id",id);
-    var span = document.createElement("span");
-    span.innerHTML = type;
-    var eliminar = document.createElement("a");
-    eliminar.setAttribute("href","#");
-    eliminar.addEventListener("click",function(e){
-        e.preventDefault();
-        var resourceId = e.target.parent.getAttribute("data-id");
-        delete this.resources[resourceId];
-
-    });
-    post.appendChild(span);
-    post.appendChild(eliminar);
-    return post;
-  }
+        });
+        post.appendChild(span);
+        post.appendChild(eliminar);
+        return post;
+    }
 }
 
+function posicionBox(box) {
+  var mov = 20;
+  var inicial = 33;
+  var boxNew = document.getElementById(box);
+  box = box.split("-");
+  boxNew.style.top = (inicial + mov * box[1])+"%";
+}
 
 window.addEventListener("load", function(){
 
-  var apopup = document.getElementById('apopup');
-  apopup.addEventListener('click',function (event) {
-    event.preventDefault();
-    document.getElementById('box').style.display = "block";
-  });
+  var popup = document.getElementsByClassName("tooltip");
+  var cerrar = document.getElementsByClassName("cerrar");
+  var adicionar = document.getElementsByClassName("add");
 
-  var close = document.getElementById("btn-close");
-  close.addEventListener('click',function(event){+
-    event.preventDefault();
-    box.style.display = "none";
-  });
+  for(var i=0; i<popup.length; i++){
+    popup[i].addEventListener('click',function (event) {
+      event.preventDefault();
+      var box = this.name;
+      posicionBox(box);
+      document.getElementById(box).style.display = "inline-block";
+    });
+  }
+  for(i=0; i<cerrar.length; i++){
+    cerrar[i].addEventListener("click", function (evt) {
+      evt.preventDefault();
+      var box = this.name;
+      document.getElementById(box).style.display = "none";
+    });
+
+  }
+
+  for(i = 0; i<adicionar.length; i++){
+
+      
+
+  }
+
+
+
+
+
+
 
 
 
